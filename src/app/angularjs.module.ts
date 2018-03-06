79b686638f1f07b5cb0451ca79184ce45dabf119
Sebastian Florian <rebardesigndk@gmail.com>
2018-03-06 14:48:33 +0100

    fix(redux): Fix redux in angularJS

--- src/app/angularjs.module.ts
+++ src/app/angularjs.module.ts
@@ -2,67 +2,12 @@ import * as angular from 'angular';
 import ngRedux from 'ng-redux';
 import 'angular-route';
 
-import { reduxConfig, runNgRedux, reduxService, AngularJsActions } from './angularjs.redux';
+import { reduxConfig, runNgRedux, ReduxService, AngularJsActions } from './angularjs.redux';
 
 export const angularJsApp = angular.module('angularJsApp', ['ngRoute', ngRedux]);
 
 angularJsApp
   .config(reduxConfig);
-
-angularJsApp
-  .run(runNgRedux);
-
-angularJsApp
-  .service('reduxService', reduxService);
-
-
-angularJsApp.component('angularJsA', {
-  controller: class {
-    actions: any = {};
-    constructor(public reduxService) {
-      reduxService.connect(null, AngularJsActions)(this.actions);
-      reduxService.subscribe(() => {
-        console.log('state is ', reduxService.getState());
-      });
-    }
-
-    increase() {
-      this.actions.increaseCounter();
-    }
-
-    decrease() {
-      this.actions.decreaseCounter();
-    }
-  },
-  template: `
-    ANGULARJS RENDERED a/ng1
-      <div>
-        <a href="/a/ng2">ANGULAR A</a>
-        <a href="/b/ng2">ANGULAR B</a>
-        <a href="/a/ng1">ANGULARJS A</a>
-        <a href="/b/ng1">ANGULARJS B</a>
-      </div>
-      <div>
-        <button ng-click="increase()">Increase</button>
-        <button ng-click="decrease()">Decrease</button>
-      </div>
-  `
-});
-
-angularJsApp.component('angularJsB', {
-  controller: class {
-    constructor() { }
-  },
-  template: `
-  ANGULARJS RENDERED b/ng1
-  <div>
-    <a href="/a/ng2">ANGULAR A</a>
-    <a href="/b/ng2">ANGULAR B</a>
-    <a href="/a/ng1">ANGULARJS A</a>
-    <a href="/b/ng1">ANGULARJS B</a>
-  </div>
-  `
-});
 
 angularJsApp.config(($locationProvider, $routeProvider) => {
   $locationProvider.html5Mode(true);
@@ -75,4 +20,68 @@ angularJsApp.config(($locationProvider, $routeProvider) => {
   $routeProvider.otherwise({ template: '' });
 });
 
+angularJsApp
+  .run(runNgRedux);
 
+angularJsApp
+  .service('ReduxService', ReduxService);
+
+
+angularJsApp.component('angularJsA', {
+  controller: class AngularJsAController {
+    counter: any;
+    redux: any = {};
+    increaseCounter;
+    decreaseCounter;
+    constructor(public $ngRedux) {
+      $ngRedux.connect(this.mapStateToThis, { ...AngularJsActions })(this.redux);
+      console.log('Initial State is ', $ngRedux.getState());
+      $ngRedux.subscribe(() => {
+        console.log('state is ', $ngRedux.getState());
+      });
+    }
+
+    mapStateToThis(state) {
+      return {
+        counter: state.counter
+      };
+    }
+
+    increase() {
+      this.redux.increaseCounter();
+    }
+
+    decrease() {
+      this.redux.decreaseCounter();
+    }
+  },
+  template: `
+    ANGULARJS RENDERED a/ng1
+      <div>
+        <a href="/a/ng2">ANGULAR A</a>
+        <a href="/b/ng2">ANGULAR B</a>
+        <a href="/a/ng1">ANGULARJS A</a>
+        <a href="/b/ng1">ANGULARJS B</a>
+      </div>
+      <div>
+        <h3>{{ $ctrl.redux.counter }}</h3>
+        <button ng-click="$ctrl.increase()">Increase</button>
+        <button ng-click="$ctrl.decrease()">Decrease</button>
+      </div>
+  `
+});
+
+angularJsApp.component('angularJsB', {
+  controller: class AngularJsBController {
+    constructor() { }
+  },
+  template: `
+  ANGULARJS RENDERED b/ng1
+  <div>
+    <a href="/a/ng2">ANGULAR A</a>
+    <a href="/b/ng2">ANGULAR B</a>
+    <a href="/a/ng1">ANGULARJS A</a>
+    <a href="/b/ng1">ANGULARJS B</a>
+  </div>
+  `
+});
--- src/app/angularjs.redux.ts
+++ src/app/angularjs.redux.ts
@@ -1,57 +1,77 @@
 import { createLogger } from 'redux-logger';
 
 const initialState = {
-  counter: 0
+    counter: 0
 };
 
 
 // REDUCER
 const AngularJsReducer = function (state = initialState, action) {
-  switch (action.type) {
-    case 'INCREASE_COUNTER': {
-      return Object.assign({}, state, {
-        counter: state.counter + 1
-      });
-    }
-    case 'DECREASE_COUNTER': {
-      return Object.assign({}, state, {
-        counter: state.counter - 1
-      });
-    }
+    switch (action.type) {
+        case 'INCREASE_COUNTER': {
+            return Object.assign({}, state, {
+                counter: state.counter + 1
+            });
+        }
+        case 'DECREASE_COUNTER': {
+            return Object.assign({}, state, {
+                counter: state.counter - 1
+            });
+        }
 
-    default:
-      return state;
-  }
+        default:
+            return state;
+    }
 };
 
 // ACTIONS
 function increaseCounter() {
-  return {
-    type: 'INCREASE_COUNTER'
-  };
+    return {
+        type: 'INCREASE_COUNTER'
+    };
 }
 
 function decreaseCounter() {
-  return {
-    type: 'DECREASE_COUNTER'
-  };
+    return {
+        type: 'DECREASE_COUNTER'
+    };
 }
 
-export function AngularJsActions() {
-  return {
+export const AngularJsActions = {
     increaseCounter,
     decreaseCounter
-  };
 }
 
 export function reduxConfig($ngReduxProvider) {
-  $ngReduxProvider.createStoreWith(AngularJsReducer, [createLogger()]);
+    $ngReduxProvider.createStoreWith(AngularJsReducer, [createLogger()], null, initialState);
+    console.log('Redux is configured');
 }
 
 export function runNgRedux($ngRedux) {
-  window['angularJsNgRedux'] = $ngRedux;
+    console.log('Putting redux on window');
+    window['angularJsNgRedux'] = $ngRedux;
 }
 
-export function reduxService($ngRedux) {
-  return $ngRedux;
+export class ReduxService {
+    constructor($ngRedux) {
+        console.log('ngRedux Service');
+        return $ngRedux;
+    }
 }
+
+
+// TODO: Find this thing I found on GitHub
+
+/*
+import myStore from '../shared/store'; // initial store setup using plain Redux
+
+function storeProviderEnhancer() {
+  return () => myStore;
+}
+
+// ... in AngularJS
+$ngReduxProvider.createStoreWith(state => state, [], [storeProviderEnhancer]);
+
+// ... in Angular using angular-redux/store
+this.ngRedux.provideStore(store as Store<IAppState>);
+*/
